0cb6089964df6bea0dd6408ace49d8dc48cd9f7d
Baawa <albin.baaw@gmail.com>
2016-09-25 14:56:11 +0200

    Push-li-push

---
+++ NotPokemonGo/backend/model/AssignmentDirector.java
@@ -0,0 +1,6 @@
+package com.le_scrum_masters.notpokemongo;
+
+public interface AssignmentDirector {
+    AssignmentItem getDailyPersonalAssignment();
+    AssignmentItem getDailyCommunityAssignment();
+}
---
+++ NotPokemonGo/backend/model/AssignmentItem.java
@@ -0,0 +1,11 @@
+package com.le_scrum_masters.notpokemongo;
+
+public interface AssignmentItem {
+    String getName();
+    Object getPosition();
+    Object getProgress();
+    Object getImage();
+
+    void setProgress();
+
+}
