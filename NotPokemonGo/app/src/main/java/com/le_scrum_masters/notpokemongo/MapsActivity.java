45ced90e52f8fca13cfb9bed22a135c86df7f1bb
Swim0n <simon.y.mare@gmail.com>
2016-09-26 16:11:57 +0200

    tja

--- NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/MapsActivity.java
+++ NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/MapsActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MapsActivity extends AppCompatActivity {
-
+// hahahahahahahah testar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
