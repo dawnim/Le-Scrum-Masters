ae7b7bfb0db175a204df9257b28300dc607478b1
David Iliefski <dij@live.se>
2016-09-26 14:45:32 +0200

    Empty Map activity and layout

---
+++ NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/MapsActivity.java
@@ -0,0 +1,15 @@
+package com.le_scrum_masters.notpokemongo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+    }
+}
