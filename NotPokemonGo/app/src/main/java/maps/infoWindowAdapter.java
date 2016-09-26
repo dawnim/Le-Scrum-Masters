21b736b6d97cfdb7fb7b0b29f86f75c55ae7572a
David Iliefski <dij@live.se>
2016-09-27 01:40:24 +0200

    Modifiable info windows for markers
    bare bones to modify info windows for markers

---
+++ NotPokemonGo/app/src/main/java/maps/infoWindowAdapter.java
@@ -0,0 +1,34 @@
+package maps;
+
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import model.NPGAssignmentItem;
+
+/**
+ * För att modifiera rutan över markers med t.ex. assignmentbilden och kanske några ords beskrivning
+ * Created by David on 2016-09-27.
+ */
+
+public class infoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private NPGAssignmentItem assignment;
+
+    public void setActiveAssignmentMarker(NPGAssignmentItem assignment){
+        this.assignment = assignment;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View view = new View(null);
+
+        return view;
+    }
+}
