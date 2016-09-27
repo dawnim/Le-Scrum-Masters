765046644a28732d11751bdf1c857a1e6b85a481
Baawa <albin.baaw@gmail.com>
2016-09-27 23:27:29 +0200

    plis merge

---
+++ NotPokemonGo/app/src/backend/model/Place.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Created by Albin on 2016-09-26.
+ */
+public interface Place {
+    public enum PlaceType{
+        PERSONAL, COMMUNITY;
+    }
+
+    int[] getCoordinates(); // index 0 = latitude, index 1 = longitude
+    PlaceType getType();
+}
