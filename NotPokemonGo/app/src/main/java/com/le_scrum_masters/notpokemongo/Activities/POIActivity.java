119adbd8aa4e2b1feceacfa450503a0a92b67588
Swim0n <simon.y.mare@gmail.com>
2016-09-30 00:25:35 +0200

    added a basic template activity for points of interest

---
+++ NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/Activities/POIActivity.java
@@ -0,0 +1,15 @@
+package com.le_scrum_masters.notpokemongo.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.le_scrum_masters.notpokemongo.R;
+
+public class POIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poi);
+    }
+}
