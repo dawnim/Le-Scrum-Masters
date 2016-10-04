0bdf2d469802a9485baedaf706226c6c8cdfe5eb
Swim0n <simon.y.mare@gmail.com>
2016-10-04 14:43:19 +0200

    Point Of Interest now transparent and overlaid on the map

--- NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/Activities/POIActivity.java
+++ NotPokemonGo/app/src/main/java/com/le_scrum_masters/notpokemongo/Activities/POIActivity.java
@@ -2,6 +2,7 @@ package com.le_scrum_masters.notpokemongo.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.le_scrum_masters.notpokemongo.R;
 
@@ -10,6 +11,9 @@ public class POIActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_poi);
+        getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT);
     }
 }
