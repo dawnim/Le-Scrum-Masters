272a2dae10199dad3646c728909e78cfbc7035a4
David Iliefski <dij@live.se>
2016-10-15 20:52:14 +0200

    MapBehaviour class updated
    Allows getting user position and centering the map on the users location
    
    Do not use the automatic updates of map, so far theyre not really
    working as intended

--- NotPokemonGo/app/src/main/java/maps/MapBehaviour.java
+++ NotPokemonGo/app/src/main/java/maps/MapBehaviour.java
@@ -22,9 +22,8 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-
-
 
 public class MapBehaviour implements
         GoogleApiClient.ConnectionCallbacks,
@@ -33,16 +32,23 @@ public class MapBehaviour implements
 
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequestHighAccuracy;
+    private LatLng lastLocation;
     private LatLng currentLocation;
     private Context contextActivity;
     private GoogleMap map;
     private boolean requestingLocationUpdates;
+    private boolean connectedToApi = false;
 
+    private static final int ANIMATE_SPEED_TURN = 1000;
+    private static final int BEARING_OFFSET = 20;
     private int updateInterval = 5000; //ms
+    private int minUpdateInterval = 3000; //ms
 
     public MapBehaviour(Context contextActivity, GoogleMap map){
         this.contextActivity = contextActivity;
         this.map = map;
+
+        connectToLocationServices();
     }
 
     public void enableLocationUpdates(){
@@ -61,16 +67,17 @@ public class MapBehaviour implements
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
         googleApiClient.connect();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        connectedToApi = true;
         locationRequestHighAccuracy = new LocationRequest()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(updateInterval)
-                .setFastestInterval(updateInterval);
+                .setFastestInterval(minUpdateInterval);
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequestHighAccuracy);
@@ -80,11 +87,13 @@ public class MapBehaviour implements
                 LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
                         builder.build());
 
+        if(requestingLocationUpdates){
             requestLocationUpdates();
+        }
     }
 
-    protected void requestLocationUpdates() {
-       requestingLocationUpdates = true;
+    private void requestLocationUpdates() {
+        requestingLocationUpdates = true;
         if (ContextCompat.checkSelfPermission(contextActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(
@@ -94,18 +103,72 @@ public class MapBehaviour implements
 
     @Override
     public void onLocationChanged(Location location) {
-        if(requestingLocationUpdates){
-        currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 20));
+
+        if(currentLocation != null){
+            lastLocation = currentLocation;
+        }
+        currentLocation =  new LatLng(location.getLatitude(),location.getLongitude());
+
+        if(lastLocation != null) {
+
+            if (requestingLocationUpdates) {
+                CameraPosition cameraPosition =
+                        new CameraPosition.Builder()
+                                .target(currentLocation)
+                                .bearing(getBearingBetweenCoords(lastLocation, currentLocation) + BEARING_OFFSET)
+                                .tilt(map.getCameraPosition().tilt)
+                                .zoom(20)
+                                .build();
+
+                map.animateCamera(
+                        CameraUpdateFactory.newCameraPosition(cameraPosition),
+                        ANIMATE_SPEED_TURN,
+                        null
+                );
+            }
         }
     }
     @Override
     public void onConnectionSuspended(int i) {
-
+        connectedToApi = false;
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        connectedToApi = false;
+    }
+    public boolean isConnectedToApi(){
+        return connectedToApi;
+    }
 
+    public void setMapPositionToCurrentLocation(){
+        if(currentLocation!=null) {
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+        }
+    }
+
+
+    public boolean isRequestingLocationUpdates(){
+        return requestingLocationUpdates;
+    }
+
+
+    public LatLng getCurrentLocation(){
+        return new LatLng(currentLocation.latitude, currentLocation.longitude);
+    }
+
+    private float getBearingBetweenCoords(LatLng start, LatLng stop){
+        double latitude1 = Math.toRadians(start.latitude);
+        double latitude2 = Math.toRadians(stop.latitude);
+        double longitude1 = start.longitude;
+        double longitude2 = stop.longitude;
+
+        double longDiff= Math.toRadians(longitude2-longitude1);
+        double y= Math.sin(longDiff)*Math.cos(latitude2);
+        double x=Math.cos(latitude1)*Math.sin(latitude2)-Math.sin(latitude1)*Math.cos(latitude2)*Math.cos(longDiff);
+        double res = (Math.toDegrees(Math.atan2(y, x))+360)%360;
+
+        float bearing = (float) res;
+        return bearing;
     }
 }
